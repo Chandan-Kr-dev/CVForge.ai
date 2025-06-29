23f07dbc4fa044b5740ef94bc572ad407383927a
Chandan-kr-dev <chandanramgar@gmail.com>
2025-06-29 09:44:34 +0530

    fixes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,10 +6,10 @@ import { CivicAuthProvider } from "@civic/auth/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CivicAuthProvider clientId={import.meta.env.VITE_CIVIC_AUTH_CLIENT_ID}>
+    
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </CivicAuthProvider>
+    
   </React.StrictMode>
 );
